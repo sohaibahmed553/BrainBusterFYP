0d9e997f6d17cf7b76346ead1ab7cdf7fbba80d0
sohaibahmed553 <sohaibahmed553@gmail.com>
2021-02-16 16:01:49 +0500

    refactor: code only

--- README.md
+++
@@ -1,2 +0,0 @@
-# fyp-app
-This repository is based on final year project app named as Brain Buster.
--- database/UPDATION_IN_TABLES.sql
+++
@@ -1,6 +0,0 @@
-#UPDATE SCORES OF STUDENTS
-UPDATE Score SET
-EasyScore = 10, MediumScore = 9, HardScore = 9 where StdID = 13 and StID = 1;
-
-UPDATE Score SET
-EasyScore = 10, MediumScore = 8 where StdID = 13 and StID = 2;
