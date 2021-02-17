84a0dcd2c38cdc3a3f4ced9b214d8a2d7bcbf75e
sohaibahmed553 <sohaibahmed553@gmail.com>
2021-02-17 12:06:38 +0500

    refactor: app js

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+var express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+var instructorRouter = require("./routes/instructor");
+var instructorCoursesRouter = require("./routes/instructorcourses");
+var adminRouter = require("./routes/admin");
+var studentRouter = require("./routes/students");
+var questionsRouter = require("./routes/questions");
+var challengesRouter = require("./routes/challenges");
+var scoresRouter = require("./routes/score");
+var stagesRouter = require("./routes/stages");
+var badgesRouter = require("./routes/badges");
+var linksRouter = require("./routes/links");
+var coursesRouter = require("./routes/courses");
+var assignedCoursesRouter = require("./routes/assignedCourses");
+var authRouter = require("./routes/auth");
+var adminAuthRouter = require("./routes/adminauth");
+var instructorAuthRouter = require("./routes/instructorauth");
+var uploadsRouter = require("./routes/uploads");
+var commentsRouter = require("./routes/comments");
+
+var app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/api", (req, res) => res.send("Api Running"));
 
@@ -8,7 +31,7 @@ app.get("/api", (req, res) => res.send("Api Running"));
 app.use(express.static(path.join(__dirname, "client/build")));
 // Anything that doesn't match the above, send back index.html
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const PORT = process.env.PORT || 4000;
