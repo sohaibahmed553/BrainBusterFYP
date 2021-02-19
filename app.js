901696fe70525463ee1ca6a8ef6e6597e34a48c7
sohaibahmed553 <sohaibahmed553@gmail.com>
2021-02-19 22:09:00 +0500

    refactor: conn removed

--- app.js
+++ app.js
@@ -60,4 +60,4 @@ const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
-const conn = require("./config/db");
+// const conn = require("./config/db");
