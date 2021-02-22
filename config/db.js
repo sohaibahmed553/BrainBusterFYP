5bbff092aa26b6d8673d177f69946bb498b86eb4
sohaibahmed553 <sohaibahmed553@gmail.com>
2021-02-22 20:29:22 +0500

    refactor: env added

--- config/db.js
+++ config/db.js
@@ -1,22 +1,16 @@
 const mysql = require("mysql");
 
 var conn = mysql.createConnection({
-  host: "35.193.167.12",
-  port: 3306,
-  user: "root",
-  password: "F81Lwqk7d5qzdGNa",
-  database: "brainbuster",
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
   multipleStatements: true,
 });
-// var conn = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "sohaib123",
-//   database: "brainbuster",
-//   multipleStatements: true,
-// });
 
 conn.connect((err) => {
+  console.log(process.env.MYSQL_HOST);
   if (err) throw err;
   console.log("Mysql Connected...");
 });
